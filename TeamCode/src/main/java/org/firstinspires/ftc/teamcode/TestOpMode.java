e77049e8e2ba61d66195d72bbfbeb42907e70c08
Mikko <neukirchm@stny.rr.com>
2017-09-10 16:06:37 -0400

    Slight changes to try to debug the no opmodes issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -2,16 +2,20 @@ package org.firstinspires.ftc.teamcode;
 
 //Created by Mikko on 2017-09-08
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+@Autonomous(name="TestOpMode", group="Test")
 public class TestOpMode extends OpMode
 {
+    @Override
     public void init()
     {
         telemetry.addData("State", "Initialized");
         telemetry.update();
     }
 
+    @Override
     public void loop()
     {
         telemetry.addData("State", "Running");
