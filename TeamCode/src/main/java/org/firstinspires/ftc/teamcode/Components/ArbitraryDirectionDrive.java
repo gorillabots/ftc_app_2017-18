529b5e6bca7160f4df97d67f4a36b5f208c79805
Mikko <neukirchm@stny.rr.com>
2017-10-24 20:40:18 -0400

    Worked on end conditions for ArbitraryDirectionDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/ArbitraryDirectionDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/ArbitraryDirectionDrive.java
@@ -3,28 +3,79 @@ package org.firstinspires.ftc.teamcode.Components;
 //Created by Mikko on 2017-10-24
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class ArbitraryDirectionDrive
 {
     public static final int N = 0, NE = 45, E = 90, SE = 135, S = 180, SW = 215, W = 270, NW = 305;
 
     OpMode opmode;
+    DcMotor m1, m2, m3, m4;
 
     public ArbitraryDirectionDrive(OpMode opmode)
     {
         this.opmode = opmode;
     }
 
-    public void drive()
+    public void drive(int direction, double speed, int magnitude) //No rotation fixing from gyro data!
     {
-        while(true)
+        double rad = Math.toRadians(direction + 45);
+
+        int m1n, m2n, m3n, m4n; //New encoder pos, updated at beginning of loop
+        int m1l = m1.getCurrentPosition(), m2l = m2.getCurrentPosition(), //Last encoder pos, updated at end of loop
+                m3l = m3.getCurrentPosition(), m4l = m4.getCurrentPosition();
+        int m1d, m2d, m3d, m4d; //Encoder pos change since last loop
+
+        while(getThingamajigger(direction) < magnitude)
         {
-            int heading = getHeadingBecauseImBadAtNavx();
+            //Update new encoder pos
+            m1n = m1.getCurrentPosition();
+            m2n = m2.getCurrentPosition();
+            m3n = m3.getCurrentPosition();
+            m4n = m4.getCurrentPosition();
+
+            //Update encoder pos delta
+            m1d = m1n - m1l;
+            m2d = m2n - m2l;
+            m3d = m3n - m3l;
+            m4d = m4n - m4l;
+
+            double m13a = (m1d + m3d) / 2d; //Average of m1 and m3
+            double m24a = (m2d + m4d) / 2d;
+
+            double length = Math.sqrt(Math.pow(m13a, 2) + Math.pow(m24a, 2));
+
+            double m13n = m13a / length; //Normalized vector
+            double m24n = m24a / length;
+
+            double m13 = Math.sin(rad) * speed;
+            double m24 = Math.cos(rad) * speed;
+
+            m1.setPower(m13);
+            m2.setPower(m24);
+            m3.setPower(m13);
+            m4.setPower(m24);
         }
+
+        m1.setPower(0);
+        m2.setPower(0);
+        m3.setPower(0);
+        m4.setPower(0);
     }
 
-    public int getHeadingBecauseImBadAtNavx()
+    double getThingamajigger(double dir)
     {
-        return 0;
+        int m1e = m1.getCurrentPosition(); //m1e = motor 1 encoder value
+        int m2e = m2.getCurrentPosition();
+        int m3e = m3.getCurrentPosition();
+        int m4e = m4.getCurrentPosition();
+
+        double m13e = (m1e + m3e) / 2d;
+        double m24e = (m2e + m4e) / 2d;
+
+        double m13 = Math.sin(dir) * m13e;
+        double m24 = Math.cos(dir) * m24e;
+
+        return Math.sqrt(Math.pow(m13, 2) + Math.pow(m24, 2));
     }
 }
