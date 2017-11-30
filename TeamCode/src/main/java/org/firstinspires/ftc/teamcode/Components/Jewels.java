1cf2b1f396f7f399714a5ae25678d5be4b3f7fa0
Mikko <neukirchm@stny.rr.com>
2017-11-29 20:15:18 -0500

    Finished Jewels crap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Jewels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Jewels.java
@@ -17,7 +17,7 @@ public class Jewels
     final double OTHER_SCAN = .5; //Scanning ??? ball
     final double OTHER_HIT_CENTER = .6; //Between the balls, ready to hit
     final double OTHER_HIT_LEFT = .6; //Hits the left jewel
-    final double OTHER_HIT_RIGHT = .6 //Hits the right jewel
+    final double OTHER_HIT_RIGHT = .6; //Hits the right jewel
     final double OTHER_ERROR = .03;
 
 
@@ -29,21 +29,23 @@ public class Jewels
     {
         baseServo = hm.servo.get("rotateArm");
         otherServo = hm.servo.get("arm");
+        color = hm.colorSensor.get("ballColor");
+        color.enableLed(false);
     }
 
     public void reset()
     {
         otherServo.setPosition(OTHER_IN); //Retract other servo
         waitForServoCloseEnough(otherServo, OTHER_IN, OTHER_ERROR);
-        baseServo.setPosition(BASE_UP);
+        baseServo.setPosition(BASE_UP); //Retract base servo
         waitForServoCloseEnough(baseServo, BASE_UP, BASE_ERROR);
     }
 
     public void scanPosition()
     {
-        baseServo.setPosition(BASE_SCAN);
+        baseServo.setPosition(BASE_SCAN); //Extend base servo
         waitForServoCloseEnough(baseServo, BASE_SCAN, BASE_ERROR);
-        otherServo.setPosition(OTHER_SCAN);
+        otherServo.setPosition(OTHER_SCAN); //Extend other servo
         waitForServoCloseEnough(otherServo, OTHER_SCAN, OTHER_ERROR);
     }
 
@@ -57,22 +59,59 @@ public class Jewels
 
     public void hitLeft()
     {
-        hitPosition();
-        otherServo.setPosition(OTHER_HIT_LEFT);
+        //hitPosition(); //Get in between the balls
+        otherServo.setPosition(OTHER_HIT_LEFT); //Hit left
         waitForServoCloseEnough(otherServo, OTHER_HIT_LEFT, OTHER_ERROR);
-        baseServo.setPosition(BASE_SCAN);
+        baseServo.setPosition(BASE_SCAN); //Lift so reset doesn't knock the other ball
         waitForServoCloseEnough(baseServo, BASE_SCAN, BASE_ERROR);
     }
 
     public void hitRight()
     {
-        hitPosition();
-        otherServo.setPosition(OTHER_HIT_RIGHT);
+        //hitPosition(); //Get in between the balls
+        otherServo.setPosition(OTHER_HIT_RIGHT); //Hit right
         waitForServoCloseEnough(otherServo, OTHER_HIT_RIGHT, OTHER_ERROR);
-        baseServo.setPosition(BASE_SCAN);
+        baseServo.setPosition(BASE_SCAN); //Lift so reset doesn't knock the other ball
         waitForServoCloseEnough(baseServo, BASE_SCAN, BASE_ERROR);
     }
 
+    public void hitRed()
+    {
+        color.enableLed(true);
+        boolean isRed = color.red() >= color.blue(); //Right ball is red
+        color.enableLed(false);
+
+        hitPosition(); //Get in position to hit
+
+        if(isRed) //Right is red, hit red on right
+        {
+            hitRight();
+        }
+        else //Right is blue, hit red on left
+        {
+            hitLeft();
+        }
+    }
+
+    public void hitBlue()
+    {
+        color.enableLed(true);
+        boolean isRed = color.red() >= color.blue(); //Right ball is red
+        color.enableLed(false);
+
+        hitPosition(); //Get in position to hit
+
+        if(isRed) //Right is red, hit blue on left
+        {
+            hitLeft();
+        }
+        else //Right is blue, hit blue on right
+        {
+            hitRed();
+        }
+    }
+
+    //Waits for the servo to be at target plus or minus an error value
     private void waitForServoCloseEnough(Servo servo, double target, double error)
     {
         double min = target - error;
@@ -91,6 +130,7 @@ public class Jewels
         }
     }
 
+    //Is a double in a range between two other doubles?
     private boolean inRange(double value, double min, double max)
     {
         return (value > min) && (value < max);
